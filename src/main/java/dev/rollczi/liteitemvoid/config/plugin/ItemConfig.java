afffc34f4e726bf06618556d50d64da77e679943
Osnixer <Osnixeroo@gmail.com>
2023-04-04 01:12:00 +0200

    Implementation lore to item guis

--- src/main/java/dev/rollczi/liteitemvoid/config/plugin/ItemConfig.java
+++ src/main/java/dev/rollczi/liteitemvoid/config/plugin/ItemConfig.java
@@ -16,6 +16,10 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import panda.utilities.StringUtils;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Contextual
 public class ItemConfig {
 
@@ -24,10 +28,16 @@ public class ItemConfig {
             .postProcessor(new LegacyColorProcessor())
             .build();
 
+    @Exclude
+    private static final TextComponent RESET = Component.text()
+            .decoration(TextDecoration.ITALIC, false)
+            .build();
+
     public Material type = Material.STONE;
     public String skull = StringUtils.EMPTY;
     public int durability = 0;
     public Component name = MINI_MESSAGE.deserialize("<green>text</green>");
+    public List<Component> lore = Collections.singletonList(MINI_MESSAGE.deserialize("<green>lore</green>"));
 
     public ItemConfig() {}
 
@@ -37,6 +47,15 @@ public class ItemConfig {
         this.name = MINI_MESSAGE.deserialize(name);
     }
 
+    public ItemConfig(Material type, int durability, String name, List<String> lore) {
+        this.type = type;
+        this.durability = durability;
+        this.name = MINI_MESSAGE.deserialize(name);
+        this.lore = lore.stream()
+                .map(MINI_MESSAGE::deserialize)
+                .collect(Collectors.toList());
+    }
+
     public ItemConfig(Material type, String skull, int durability, Component name) {
         this.type = type;
         this.skull = skull;
@@ -44,25 +63,39 @@ public class ItemConfig {
         this.name = name;
     }
 
-    public ItemStack toItem() {
-         TextComponent RESET = Component.text()
-                 .decoration(TextDecoration.ITALIC, false)
-                 .build();
+    public ItemConfig(Material type, String skull, int durability, Component name, List<Component> lore) {
+        this.type = type;
+        this.skull = skull;
+        this.durability = durability;
+        this.name = name;
+        this.lore = lore;
+    }
 
+    public ItemStack toItem() {
         if (skull.isEmpty()) {
             return ItemBuilder.from(new ItemStack(type, 1, (short) durability))
                     .name(RESET.append(name))
+                    .lore(lore.stream()
+                            .map(RESET::append)
+                            .collect(Collectors.toList()))
                     .build();
         }
 
         return ItemBuilder.skull()
                 .texture(skull)
                 .name(RESET.append(name))
+                .lore(lore.stream()
+                        .map(RESET::append)
+                        .collect(Collectors.toList()))
                 .build();
     }
 
     public static ItemConfig skull(String skull, Component name) {
         return new ItemConfig(Material.STONE, skull, 0, name);
+    }
+
+    public static ItemConfig skull(String skull, Component name, List<Component> lore) {
+        return new ItemConfig(Material.STONE, skull, 0, name, lore);
     }
 
 }

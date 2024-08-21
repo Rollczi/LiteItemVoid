8aa19c724f98a463c7e25c87c150cd1d662b2888
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-08-21 18:16:56 +0200

    Update shadow jar to gradleup version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `java-library`
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("xyz.jpenilla.run-paper") version "2.2.2"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "dev.rollczi"
